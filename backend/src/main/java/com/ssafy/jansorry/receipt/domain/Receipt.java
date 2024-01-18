f9608345b90d14e5f4b5b8a1ffbb0ff8c0dfdd12
takealook97 <takealook97@naver.com>
2024-01-18 13:47:37 +0900

    feat: add Receipt entity

--- backend/src/main/java/com/ssafy/jansorry/receipt/domain/Receipt.java
+++ backend/src/main/java/com/ssafy/jansorry/receipt/domain/Receipt.java
@@ -1,4 +1,39 @@
 package com.ssafy.jansorry.receipt.domain;
 
-public class Receipt {
+import com.ssafy.jansorry.common.BaseEntity;
+import com.ssafy.jansorry.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Receipt extends BaseEntity {
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@NonNull
+	private String title;
+	@NonNull
+	private String description;
+	@NonNull
+	private String message;
+	@NonNull
+	private String familyUrl;
+	@NonNull
+	private String friendUrl;
+	@NonNull
+	private Boolean deleted;
 }
