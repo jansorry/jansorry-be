a2aa7f695f4114e10188425ec7adeb31dee74467
takealook97 <takealook97@naver.com>
2024-01-18 13:51:18 +0900

    feat: add follow entity

--- backend/src/main/java/com/ssafy/jansorry/follow/domain/Follow.java
+++ backend/src/main/java/com/ssafy/jansorry/follow/domain/Follow.java
@@ -1,4 +1,29 @@
 package com.ssafy.jansorry.follow.domain;
 
-public class Follow {
+import com.ssafy.jansorry.common.BaseEntity;
+import com.ssafy.jansorry.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Follow extends BaseEntity {
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@NonNull
+	private Long toId;
 }
