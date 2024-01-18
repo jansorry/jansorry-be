45bf29436b36ee19c29e3d289ff97a51bda1514a
takealook97 <takealook97@naver.com>
2024-01-18 13:50:18 +0900

    feat: add action domain with service, controller etc

---
+++ backend/src/main/java/com/ssafy/jansorry/action/domain/Action.java
@@ -0,0 +1,44 @@
+package com.ssafy.jansorry.action.domain;
+
+import com.ssafy.jansorry.common.BaseEntity;
+import com.ssafy.jansorry.favorite.domain.Favorite;
+import com.ssafy.jansorry.member.domain.Member;
+import com.ssafy.jansorry.nag.domain.Nag;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Action extends BaseEntity {
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "nag_id")
+	private Nag nag;
+
+	@OneToMany(mappedBy = "action", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	@Builder.Default
+	private List<Favorite> favorites = new ArrayList<>();
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@NonNull
+	private String content;
+}
