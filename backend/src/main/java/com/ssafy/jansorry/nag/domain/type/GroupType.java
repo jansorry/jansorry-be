b8f587b4efcea556b2c531eb7df11c8ddd35c915
takealook97 <takealook97@naver.com>
2024-01-18 13:51:54 +0900

    feat: add group type constant

---
+++ backend/src/main/java/com/ssafy/jansorry/nag/domain/type/GroupType.java
@@ -0,0 +1,17 @@
+package com.ssafy.jansorry.nag.domain.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum GroupType {
+	LEARNING(0L),
+	APPEARANCE(1L),
+	LOVE(2L),
+	CAREER(3L),
+	FINANCE(4L),
+	OTHER(5L);
+
+	private final Long value;
+}
