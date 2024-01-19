1c16694252c0d517d374267181a91398fece1083
takealook97 <takealook97@naver.com>
2024-01-19 13:15:41 +0900

    feat: add nag service class
    set find detail method through mapper
    
    Related to: #S10P12A107-51

--- backend/src/main/java/com/ssafy/jansorry/nag/service/NagService.java
+++ backend/src/main/java/com/ssafy/jansorry/nag/service/NagService.java
@@ -1,4 +1,24 @@
 package com.ssafy.jansorry.nag.service;
 
+import static com.ssafy.jansorry.nag.util.NagMapper.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.jansorry.nag.domain.Nag;
+import com.ssafy.jansorry.nag.dto.NagDetailDto;
+import com.ssafy.jansorry.nag.repository.NagRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class NagService {
+	private final NagRepository nagRepository;
+
+	public NagDetailDto findNagDetail(Long nagId) {
+		Nag nag = nagRepository.findNagById(nagId).orElseThrow(RuntimeException::new);
+		return toNagDetailDto(nag);
+	}
 }
