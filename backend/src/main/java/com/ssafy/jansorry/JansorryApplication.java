57d6667e072158bd15c372cc2c38b6b8ded5a4dc
takealook97 <takealook97@naver.com>
2024-01-19 17:29:07 +0900

    refactor: datetime 소수점 삭제 및 enable jpa auditing 추가

--- backend/src/main/java/com/ssafy/jansorry/JansorryApplication.java
+++ backend/src/main/java/com/ssafy/jansorry/JansorryApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.jansorry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JansorryApplication {
 
 	public static void main(String[] args) {
