c941192f24eec7f36c63e243865800f81f7f901e
takealook97 <takealook97@naver.com>
2024-01-19 13:19:32 +0900

    feat: add nag controller
    set get nag detail method
    
    Related to: #S10P12A107-51

--- backend/src/main/java/com/ssafy/jansorry/nag/controller/NagController.java
+++ backend/src/main/java/com/ssafy/jansorry/nag/controller/NagController.java
@@ -1,4 +1,26 @@
 package com.ssafy.jansorry.nag.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.jansorry.nag.dto.NagDetailDto;
+import com.ssafy.jansorry.nag.service.NagService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/nags")
 public class NagController {
+	private final NagService nagService;
+
+	@ResponseBody
+	@GetMapping("/{nagId}")
+	private ResponseEntity<NagDetailDto> getNagDetail(@PathVariable Long nagId) {
+		return ResponseEntity.ok(nagService.findNagDetail(nagId));
+	}
 }
