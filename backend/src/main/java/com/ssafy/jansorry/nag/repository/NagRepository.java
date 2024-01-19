6a0facbbbd95a32ad568a89b95a7626a0b48edf6
takealook97 <takealook97@naver.com>
2024-01-19 14:05:27 +0900

    feat: add readAll method on nag service
    Related to: #S10P12A107-51

--- backend/src/main/java/com/ssafy/jansorry/nag/repository/NagRepository.java
+++ backend/src/main/java/com/ssafy/jansorry/nag/repository/NagRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.jansorry.nag.domain.Nag;
-import com.ssafy.jansorry.nag.dto.NagDto;
 
 @Repository
 public interface NagRepository extends JpaRepository<Nag, Long> {
--- backend/src/main/java/com/ssafy/jansorry/nag/service/NagService.java
+++ backend/src/main/java/com/ssafy/jansorry/nag/service/NagService.java
@@ -2,12 +2,16 @@ package com.ssafy.jansorry.nag.service;
 
 import static com.ssafy.jansorry.nag.util.NagMapper.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.jansorry.nag.domain.Nag;
 import com.ssafy.jansorry.nag.dto.NagDto;
 import com.ssafy.jansorry.nag.repository.NagRepository;
+import com.ssafy.jansorry.nag.util.NagMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,8 +21,15 @@ import lombok.RequiredArgsConstructor;
 public class NagService {
 	private final NagRepository nagRepository;
 
-	public NagDto findNagDetail(Long nagId) {
+	public NagDto readNag(Long nagId) {
 		Nag nag = nagRepository.findNagById(nagId).orElseThrow(RuntimeException::new);
 		return toDto(nag);
+	}
+
+	public List<NagDto> readAllNags() {
+		return nagRepository.findAllByDeletedFalse()
+			.stream()
+			.map(NagMapper::toDto)
+			.collect(Collectors.toList());
 	}
 }
