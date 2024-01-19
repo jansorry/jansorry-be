a0fdcc45684604349937da7502400465e89ef94c
takealook97 <takealook97@naver.com>
2024-01-19 13:15:10 +0900

    feat: add nag repository
    set find method with id
    
    Related to: #S10P12A107-51

--- backend/src/main/java/com/ssafy/jansorry/nag/repository/NagRepository.java
+++ backend/src/main/java/com/ssafy/jansorry/nag/repository/NagRepository.java
@@ -1,4 +1,17 @@
 package com.ssafy.jansorry.nag.repository;
 
-public class NagRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.jansorry.nag.domain.Nag;
+import com.ssafy.jansorry.nag.dto.NagDto;
+
+@Repository
+public interface NagRepository extends JpaRepository<Nag, Long> {
+	Optional<Nag> findNagById(Long nagId);
+
+	List<NagDto> findAllByDeletedFalse();
 }
