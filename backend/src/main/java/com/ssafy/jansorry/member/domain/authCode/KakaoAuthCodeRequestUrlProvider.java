8db2c5f820baee39503a04ed1343897ff6915401
sunju5402 <ssonju5402@gmail.com>
2024-01-21 18:35:37 +0900

    feat: kakao 로그인 api 통신
    Related to: #S10P12A107-67

---
+++ backend/src/main/java/com/ssafy/jansorry/member/domain/authCode/KakaoAuthCodeRequestUrlProvider.java
@@ -0,0 +1,29 @@
+package com.ssafy.jansorry.member.domain.authCode;
+
+import com.ssafy.jansorry.config.KakaoOauthConfig;
+import com.ssafy.jansorry.member.domain.type.OauthServerType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@RequiredArgsConstructor
+public class KakaoAuthCodeRequestUrlProvider implements AuthCodeRequestUrlProvider {
+	private final KakaoOauthConfig kakaoOauthConfig;
+
+
+	@Override
+	public OauthServerType supportServer() {
+		return OauthServerType.KAKAO;
+	}
+
+	@Override
+	public String provide() {
+		return UriComponentsBuilder
+			.fromUriString("https://kauth.kakao.com/oauth/authorize")
+			.queryParam("response_type", "code")
+			.queryParam("client_id", kakaoOauthConfig.clientId())
+			.queryParam("redirect_uri", kakaoOauthConfig.redirectUri())
+			.toUriString();
+	}
+}
