78f6459a49f11441401a6a920b26c75dcf85ff1e
sunju5402 <ssonju5402@gmail.com>
2024-01-21 18:27:30 +0900

    feat: 시간 자동완성 및 properties scan annotation 추가
    Related to: #S10P12A107-67

--- backend/src/main/java/com/ssafy/jansorry/JansorryApplication.java
+++ backend/src/main/java/com/ssafy/jansorry/JansorryApplication.java
@@ -2,8 +2,13 @@ package com.ssafy.jansorry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class JansorryApplication {
 
 	public static void main(String[] args) {
