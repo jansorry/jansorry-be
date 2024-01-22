dccaba74263c635a2e4f3a60e323fd0e1f58e15b
ozllzlme <jin_in_1992@naver.com>
2024-01-22 17:00:03 +0900

    feat: Add receipt save in repository
    Related to: #S10P12A107-148

--- backend/src/main/java/com/ssafy/jansorry/receipt/repository/ReceiptRepository.java
+++ backend/src/main/java/com/ssafy/jansorry/receipt/repository/ReceiptRepository.java
@@ -1,4 +1,18 @@
 package com.ssafy.jansorry.receipt.repository;
 
-public class ReceiptRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.jansorry.member.domain.Member;
+import com.ssafy.jansorry.receipt.domain.Receipt;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+public interface ReceiptRepository extends JpaRepository<Receipt,Long> {
+	//JpaRepository: PagingAndSortingRepository, QueryByExampleExecutor 인터페이스를 상속받고 있음
+	//PagingAndSqortingRepository: CrudRepository 인터페이스를 상속받고 있음
+	List<Receipt> findAllByMemberIdAndDeletedFalse(Long memberId);
 }
