17dbdb4febc4ae770de74d9289d888b2069511cd
sunju5402 <ssonju5402@gmail.com>
2024-01-22 11:41:06 +0900

    feat: 회원가입 api 추가
    Related to: #S10P12A107-66

--- backend/src/main/java/com/ssafy/jansorry/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/jansorry/member/controller/MemberController.java
@@ -1,18 +1,24 @@
 package com.ssafy.jansorry.member.controller;
 
+import com.ssafy.jansorry.member.dto.SignUpRequest;
+import com.ssafy.jansorry.member.dto.SignUpResponse;
+import com.ssafy.jansorry.member.service.MemberService;
 import com.ssafy.jansorry.member.service.TokenService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/members")
 public class MemberController {
+	private final MemberService memberService;
 	private final TokenService tokenService;
 
 	@GetMapping("/reissue")
@@ -22,5 +28,12 @@ public class MemberController {
 		response.setHeader("Authorization", accessToken);
 
 		return ResponseEntity.ok().build();
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<SignUpResponse> addMember(
+		@RequestBody SignUpRequest request
+	) {
+		return ResponseEntity.ok(memberService.createMember(request));
 	}
 }
