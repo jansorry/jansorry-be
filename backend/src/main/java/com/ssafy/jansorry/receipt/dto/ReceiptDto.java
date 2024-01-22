e844170cb56c413803ad9e0cbda6e0bef62da33e
ozllzlme <jin_in_1992@naver.com>
2024-01-22 16:55:19 +0900

    feat: Add Dto ReceiptDto.java
    Related to: #S10P12A107-148

--- backend/src/main/java/com/ssafy/jansorry/receipt/dto/ReceiptDto.java
+++ backend/src/main/java/com/ssafy/jansorry/receipt/dto/ReceiptDto.java
@@ -1,4 +1,13 @@
 package com.ssafy.jansorry.receipt.dto;
 
-public class ReceiptDto {
+import lombok.Builder;
+
+@Builder
+public record ReceiptDto (
+	//record
+	String title,
+	String description,
+	String message,
+	String familyUrl,
+	String friendUrl) {
 }
