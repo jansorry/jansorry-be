c8d69df2b4aa4892f316264f1c2d6256aa80b284
ozllzlme <jin_in_1992@naver.com>
2024-01-22 16:59:48 +0900

    feat: Add receipt save in service
    Related to: #S10P12A107-148

--- backend/src/main/java/com/ssafy/jansorry/receipt/service/ReceiptService.java
+++ backend/src/main/java/com/ssafy/jansorry/receipt/service/ReceiptService.java
@@ -1,4 +1,27 @@
 package com.ssafy.jansorry.receipt.service;
 
+import static com.ssafy.jansorry.receipt.util.ReceiptMapper.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.jansorry.receipt.domain.Receipt;
+import com.ssafy.jansorry.receipt.dto.ReceiptDto;
+import com.ssafy.jansorry.receipt.repository.ReceiptRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class ReceiptService {
+
+	private final ReceiptRepository receiptRepository;
+
+	//Dto는 Service <-> Controller
+	//Entity는 Repository <-> Service
+	public void save(ReceiptDto receiptDto) {
+		receiptRepository.save(toEntity(receiptDto));
+	}
+
 }
