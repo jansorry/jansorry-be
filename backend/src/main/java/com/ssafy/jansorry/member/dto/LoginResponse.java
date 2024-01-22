b8c658ec284165986271102aa8b3923e54ae1834
sunju5402 <ssonju5402@gmail.com>
2024-01-22 12:42:10 +0900

    fix: class -> record로 바꾸면서 발생한 오류 수정

--- backend/src/main/java/com/ssafy/jansorry/member/dto/LoginResponse.java
+++ backend/src/main/java/com/ssafy/jansorry/member/dto/LoginResponse.java
@@ -1,12 +1,8 @@
 package com.ssafy.jansorry.member.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-@AllArgsConstructor
 public record LoginResponse(
 	Long memberId,
 	String nickname,
