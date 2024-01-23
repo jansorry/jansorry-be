f7417e410593c1e228406acbf69f8a7b09ab6a0d
takealook97 <takealook97@naver.com>
2024-01-23 15:29:08 +0900

    feat: 대응 상세조회 및 삭제 메서드 생성 및 구현
    Related to: #S10P12A107-166,
    Related to: #S10P12A107-167

--- backend/src/main/java/com/ssafy/jansorry/action/service/ActionService.java
+++ backend/src/main/java/com/ssafy/jansorry/action/service/ActionService.java
@@ -1,12 +1,16 @@
 package com.ssafy.jansorry.action.service;
 
 import static com.ssafy.jansorry.action.util.ActionMapper.*;
+import static com.ssafy.jansorry.exception.ErrorCode.*;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.jansorry.action.domain.Action;
 import com.ssafy.jansorry.action.dto.ActionCreationDto;
+import com.ssafy.jansorry.action.dto.ActionDto;
 import com.ssafy.jansorry.action.repository.ActionRepository;
+import com.ssafy.jansorry.exception.BaseException;
 import com.ssafy.jansorry.member.domain.Member;
 import com.ssafy.jansorry.nag.domain.Nag;
 import com.ssafy.jansorry.nag.repository.NagRepository;
@@ -21,7 +25,23 @@ public class ActionService {
 	private final ActionRepository actionRepository;
 
 	public void createAction(Long nagId, Member member, ActionCreationDto actionCreationDto) {
-		Nag nag = nagRepository.findNagById(nagId).orElseThrow(RuntimeException::new);// exception
+		Nag nag = nagRepository.findNagById(nagId)
+			.orElseThrow(() -> new BaseException(NAG_NOT_FOUND));
 		actionRepository.save(toEntity(nag, member, actionCreationDto));
+	}
+
+	public ActionDto readAction(Long actionId) {
+		Action action = actionRepository.findActionByIdAndDeletedFalse(actionId)
+			.orElseThrow(() -> new BaseException(ACTION_NOT_FOUND));
+		return toDto(action);
+	}
+
+	public void deleteAction(Long actionId) {
+		Action action = actionRepository.findActionById(actionId)
+			.orElseThrow(() -> new BaseException(ACTION_NOT_FOUND));
+		if (action.getDeleted()) {
+			throw new BaseException(ACTION_ALREADY_DELETED);
+		}
+		action.setDeleted(true);// dirty checking
 	}
 }
