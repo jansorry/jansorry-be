3d7ca907807e264c10457652c99512af094f3e63
takealook97 <takealook97@naver.com>
2024-01-23 11:27:31 +0900

    feat: 대응 관련 service 클래스 생성 및 대응 생성 메서드 추가
    Related to: #S10P12A107-164

--- backend/src/main/java/com/ssafy/jansorry/action/service/ActionService.java
+++ backend/src/main/java/com/ssafy/jansorry/action/service/ActionService.java
@@ -1,4 +1,27 @@
 package com.ssafy.jansorry.action.service;
 
+import static com.ssafy.jansorry.action.util.ActionMapper.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.jansorry.action.dto.ActionCreationDto;
+import com.ssafy.jansorry.action.repository.ActionRepository;
+import com.ssafy.jansorry.member.domain.Member;
+import com.ssafy.jansorry.nag.domain.Nag;
+import com.ssafy.jansorry.nag.repository.NagRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class ActionService {
+	private final NagRepository nagRepository;
+	private final ActionRepository actionRepository;
+
+	public void createAction(Long nagId, Member member, ActionCreationDto actionCreationDto) {
+		Nag nag = nagRepository.findNagById(nagId).orElseThrow(RuntimeException::new);// exception
+		actionRepository.save(toEntity(nag, member, actionCreationDto));
+	}
 }
