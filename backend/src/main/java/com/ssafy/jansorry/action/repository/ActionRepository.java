7d5a4e93613b3489f33f05fb90016cc01171d909
takealook97 <takealook97@naver.com>
2024-01-23 15:38:42 +0900

    feat: action repository 에 memberId 기반으로 작성 대응 전체 조회 메서드 추가
    Related to: #S10P12A107-165

--- backend/src/main/java/com/ssafy/jansorry/action/repository/ActionRepository.java
+++ backend/src/main/java/com/ssafy/jansorry/action/repository/ActionRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.jansorry.action.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,8 @@ import com.ssafy.jansorry.action.domain.Action;
 
 @Repository
 public interface ActionRepository extends JpaRepository<Action, Long> {
+	List<Action> findAllByMemberIdAndDeletedFalse(Long memberId);
+
 	Optional<Action> findActionByIdAndDeletedFalse(Long actionId);
 
 	Optional<Action> findActionById(Long actionId);
