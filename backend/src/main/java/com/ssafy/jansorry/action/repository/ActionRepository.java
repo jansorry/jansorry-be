7e52ff97acfda64c4604ee46c52a93b3b0da852d
takealook97 <takealook97@naver.com>
2024-01-23 11:27:11 +0900

    feat: 대응 repository 생성
    Related to: #S10P12A107-164

--- backend/src/main/java/com/ssafy/jansorry/action/repository/ActionRepository.java
+++ backend/src/main/java/com/ssafy/jansorry/action/repository/ActionRepository.java
@@ -1,4 +1,11 @@
 package com.ssafy.jansorry.action.repository;
 
-public class ActionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.jansorry.action.domain.Action;
+
+@Repository
+public interface ActionRepository extends JpaRepository<Action, Long> {
+
 }
