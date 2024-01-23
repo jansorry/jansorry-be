b6e962ae99a9361cc331fa0b88344687389074ba
takealook97 <takealook97@naver.com>
2024-01-23 11:23:23 +0900

    feat: 대응 관련 mapper 생성 및 to entity 메서드 생성
    Related to: #S10P12A107-164

--- backend/src/main/java/com/ssafy/jansorry/action/util/ActionMapper.java
+++ backend/src/main/java/com/ssafy/jansorry/action/util/ActionMapper.java
@@ -1,4 +1,23 @@
 package com.ssafy.jansorry.action.util;
 
+import com.ssafy.jansorry.action.domain.Action;
+import com.ssafy.jansorry.action.dto.ActionCreationDto;
+import com.ssafy.jansorry.action.dto.ActionDto;
+import com.ssafy.jansorry.member.domain.Member;
+import com.ssafy.jansorry.nag.domain.Nag;
+
 public class ActionMapper {
+	public static Action toEntity(Nag nag, Member member, ActionCreationDto actionCreationDto) {
+		return Action.builder()
+			.member(member)
+			.nag(nag)
+			.content(actionCreationDto.content())
+			.build();
+	}
+
+	public static ActionDto toDto() {
+		return ActionDto.builder()
+			// add field
+			.build();
+	}
 }
