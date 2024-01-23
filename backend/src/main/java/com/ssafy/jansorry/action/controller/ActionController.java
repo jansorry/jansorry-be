d1f4e3feaa594e0f856df1ee2796ae051a27bd4f
takealook97 <takealook97@naver.com>
2024-01-23 11:09:40 +0900

    feat: action 컨트롤러 생성 및 대응 생성 api 추가
    Related to: #S10P12A107-164

--- backend/src/main/java/com/ssafy/jansorry/action/controller/ActionController.java
+++ backend/src/main/java/com/ssafy/jansorry/action/controller/ActionController.java
@@ -1,4 +1,32 @@
 package com.ssafy.jansorry.action.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.jansorry.action.dto.ActionCreationDto;
+import com.ssafy.jansorry.action.service.ActionService;
+import com.ssafy.jansorry.member.domain.Member;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class ActionController {
+	private final ActionService actionService;
+
+	@PostMapping("/nags/{nagId}/actions")
+	private ResponseEntity<Void> addAction(
+		@PathVariable Long nagId,
+		@AuthenticationPrincipal Member member,
+		@RequestBody ActionCreationDto actionCreationDto
+		) {
+		actionService.createAction(nagId, member, actionCreationDto);
+		return ResponseEntity.ok().build();
+	}
 }
