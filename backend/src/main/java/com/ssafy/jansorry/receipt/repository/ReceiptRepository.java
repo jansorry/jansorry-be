6b443408a3bdb29f3d893fc06cd65b504af166da
ozllzlme <jin_in_1992@naver.com>
2024-01-23 17:30:31 +0900

    refactor: id 기준으로 정렬을 진행함
    Related to: #S10P12A107-154

--- backend/src/main/java/com/ssafy/jansorry/receipt/repository/ReceiptRepository.java
+++ backend/src/main/java/com/ssafy/jansorry/receipt/repository/ReceiptRepository.java
@@ -5,14 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ssafy.jansorry.member.domain.Member;
 import com.ssafy.jansorry.receipt.domain.Receipt;
-
-import lombok.RequiredArgsConstructor;
 
 @Repository
 public interface ReceiptRepository extends JpaRepository<Receipt,Long> {
 	//JpaRepository: PagingAndSortingRepository, QueryByExampleExecutor 인터페이스를 상속받고 있음
 	//PagingAndSqortingRepository: CrudRepository 인터페이스를 상속받고 있음
-	List<Receipt> findAllByMemberIdAndDeletedFalseOrderByCreatedAtAsc(Long memberId);
+	List<Receipt> findAllByMemberIdAndDeletedFalseOrderById(Long memberId);
 }
