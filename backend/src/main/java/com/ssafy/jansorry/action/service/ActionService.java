aa7648ad9083cc6ed8911b52f0ebff3198f7c99e
takealook97 <takealook97@naver.com>
2024-01-23 15:39:09 +0900

    feat: action service 에 memberId 기반 대응 전체 조회 메서드 추가
    Related to: #S10P12A107-165

--- backend/src/main/java/com/ssafy/jansorry/action/service/ActionService.java
+++ backend/src/main/java/com/ssafy/jansorry/action/service/ActionService.java
@@ -3,6 +3,9 @@ package com.ssafy.jansorry.action.service;
 import static com.ssafy.jansorry.action.util.ActionMapper.*;
 import static com.ssafy.jansorry.exception.ErrorCode.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +13,7 @@ import com.ssafy.jansorry.action.domain.Action;
 import com.ssafy.jansorry.action.dto.ActionCreationDto;
 import com.ssafy.jansorry.action.dto.ActionDto;
 import com.ssafy.jansorry.action.repository.ActionRepository;
+import com.ssafy.jansorry.action.util.ActionMapper;
 import com.ssafy.jansorry.exception.BaseException;
 import com.ssafy.jansorry.member.domain.Member;
 import com.ssafy.jansorry.nag.domain.Nag;
@@ -23,6 +27,13 @@ import lombok.RequiredArgsConstructor;
 public class ActionService {
 	private final NagRepository nagRepository;
 	private final ActionRepository actionRepository;
+
+	public List<ActionDto> readAllActions(Long memberId) {
+		return actionRepository.findAllByMemberIdAndDeletedFalse(memberId)
+			.stream()
+			.map(ActionMapper::toDto)
+			.collect(Collectors.toList());
+	}
 
 	public void createAction(Long nagId, Member member, ActionCreationDto actionCreationDto) {
 		Nag nag = nagRepository.findNagById(nagId)
