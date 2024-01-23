befaf14677be6a5e31950d855c4cb8b5b1a05736
takealook97 <takealook97@naver.com>
2024-01-23 17:10:35 +0900

    fix: 잔소리 조회 시 유저권한 확인

--- backend/src/main/java/com/ssafy/jansorry/nag/controller/NagController.java
+++ backend/src/main/java/com/ssafy/jansorry/nag/controller/NagController.java
@@ -3,11 +3,13 @@ package com.ssafy.jansorry.nag.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.jansorry.member.domain.Member;
 import com.ssafy.jansorry.nag.dto.NagDto;
 import com.ssafy.jansorry.nag.service.NagService;
 
@@ -20,12 +22,17 @@ public class NagController {
 	private final NagService nagService;
 
 	@GetMapping("/{nagId}")
-	private ResponseEntity<NagDto> getNag(@PathVariable Long nagId) {
+	private ResponseEntity<NagDto> getNag(
+		@PathVariable Long nagId,
+		@AuthenticationPrincipal Member member
+	) {
 		return ResponseEntity.ok(nagService.readNag(nagId));
 	}
 
 	@GetMapping
-	private ResponseEntity<List<NagDto>> getAllNags() {
+	private ResponseEntity<List<NagDto>> getAllNags(
+		@AuthenticationPrincipal Member member
+	) {
 		return ResponseEntity.ok(nagService.readAllNags());
 	}
 }
