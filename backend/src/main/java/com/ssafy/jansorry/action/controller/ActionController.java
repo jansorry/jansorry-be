eca0a1b799aa1cbf5f72c4680f68fea1e79a4e90
takealook97 <takealook97@naver.com>
2024-01-23 15:24:15 +0900

    feat: 대응 컨트롤러 삭제 및 상세조회 메서드 추가
    Related to: #S10P12A107-166,
    Related to: #S10P12A107-167

--- backend/src/main/java/com/ssafy/jansorry/action/controller/ActionController.java
+++ backend/src/main/java/com/ssafy/jansorry/action/controller/ActionController.java
@@ -2,6 +2,8 @@ package com.ssafy.jansorry.action.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.jansorry.action.dto.ActionCreationDto;
+import com.ssafy.jansorry.action.dto.ActionDto;
 import com.ssafy.jansorry.action.service.ActionService;
 import com.ssafy.jansorry.member.domain.Member;
 
@@ -25,8 +28,25 @@ public class ActionController {
 		@PathVariable Long nagId,
 		@AuthenticationPrincipal Member member,
 		@RequestBody ActionCreationDto actionCreationDto
-		) {
+	) {
 		actionService.createAction(nagId, member, actionCreationDto);
+		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("/actions/{actionId}")
+	private ResponseEntity<ActionDto> getAction(
+		@PathVariable Long actionId,
+		@AuthenticationPrincipal Member member
+	) {
+		return ResponseEntity.ok(actionService.readAction(actionId));
+	}
+
+	@DeleteMapping("/actions/{actionId}")
+	private ResponseEntity<Void> removeAction(
+		@PathVariable Long actionId,
+		@AuthenticationPrincipal Member member
+	) {
+		actionService.deleteAction(actionId);
 		return ResponseEntity.ok().build();
 	}
 }
