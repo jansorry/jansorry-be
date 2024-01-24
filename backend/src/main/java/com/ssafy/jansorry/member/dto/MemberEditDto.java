540e71a04fb41f29e79c637dd519327c36b2187e
sunju5402 <ssonju5402@gmail.com>
2024-01-24 16:14:35 +0900

    refactor: error 메세지 수정
    Related to: #S10P12A107-208

--- backend/src/main/java/com/ssafy/jansorry/member/dto/MemberEditDto.java
+++ backend/src/main/java/com/ssafy/jansorry/member/dto/MemberEditDto.java
@@ -6,8 +6,8 @@ import lombok.Builder;
 
 @Builder
 public record MemberEditDto(
-	@Size(min = 1, max = 20, message = "잘못된 요청 형식입니다. nickname은 20글자 이하입니다.")
-	@Pattern(regexp = "^[a-zA-Z0-9가-힣\\\\-\\\\_]*$", message = "잘못된 요청 형식입니다. 특수문자(~!@#$%^&*())와 공백을 제외해야 합니다,")
+	@Size(min = 1, max = 20, message = "잘못된 요청 형식입니다. nickname은 1글자 이상 20글자 이하입니다.")
+	@Pattern(regexp = "^[a-zA-Z0-9가-힣\\\\-\\\\_]*$", message = "잘못된 요청 형식입니다. 특수문자(~!@#$%^&*())와 공백을 제외해야 합니다.")
 	String nickname
 ) {
 
