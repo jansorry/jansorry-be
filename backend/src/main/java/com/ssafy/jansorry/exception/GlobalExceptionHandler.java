e9ab53672aeaa6ff4e7289fd2bce36dc2bdd847e
sunju5402 <ssonju5402@gmail.com>
2024-01-24 16:15:00 +0900

    feat: validation error custom 처리
    Related to: #S10P12A107-208

--- backend/src/main/java/com/ssafy/jansorry/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ssafy/jansorry/exception/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.ssafy.jansorry.exception;
 
+import static com.ssafy.jansorry.exception.ErrorCode.*;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,5 +14,13 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<ErrorResponse> handleBaseException(BaseException ex) {
 		return ResponseEntity.status(ex.getErrorCode().getErrorCode())
 			.body(new ErrorResponse(ex.getErrorCode().getErrorCode(), ex.getErrorCode().getErrorMsg()));
+	}
+
+	// 유효성 검사 base 예외 처리에 맞게 custom
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+		MethodArgumentNotValidException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(new ErrorResponse(METHOD_ARGUMENT_NOT_VALID.getErrorCode(), e.getFieldError().getDefaultMessage()));
 	}
 }
