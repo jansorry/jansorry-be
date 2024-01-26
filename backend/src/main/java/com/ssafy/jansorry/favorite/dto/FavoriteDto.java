63eaadaf9de57a9d3ae3ae5d35df4ccc827e490c
takealook97 <takealook97@naver.com>
2024-01-26 12:14:40 +0900

    feat: FavoriteDto 생성 및 구현

--- backend/src/main/java/com/ssafy/jansorry/favorite/dto/FavoriteDto.java
+++ backend/src/main/java/com/ssafy/jansorry/favorite/dto/FavoriteDto.java
@@ -1,5 +1,12 @@
 package com.ssafy.jansorry.favorite.dto;
 
-public class FavoriteDto {
+import java.time.LocalDateTime;
+import java.util.Set;
 
+import lombok.Builder;
+
+@Builder
+public record FavoriteDto(
+	Set<Long> memberIdSet,
+	LocalDateTime updatedAt) {
 }
