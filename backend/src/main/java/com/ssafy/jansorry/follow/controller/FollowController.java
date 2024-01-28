b7c7abb0d4deb83f0d144f4b7492b3ac555b1977
takealook97 <takealook97@naver.com>
2024-01-29 02:04:53 +0900

    refactor: follow controller 수정
    리팩토링한 service 레이어에 맞추어 전반적인 메서드들을 수정하고 추가하였다.

--- backend/src/main/java/com/ssafy/jansorry/follow/controller/FollowController.java
+++ backend/src/main/java/com/ssafy/jansorry/follow/controller/FollowController.java
@@ -3,12 +3,14 @@ package com.ssafy.jansorry.follow.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.jansorry.follow.dto.FollowCountDto;
 import com.ssafy.jansorry.follow.dto.MemberSearchResponse;
 import com.ssafy.jansorry.follow.service.FollowService;
 import com.ssafy.jansorry.member.domain.Member;
@@ -19,26 +21,34 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1/follows")
 public class FollowController {
-
 	private final FollowService followService;
 
-	@PostMapping()
-	public ResponseEntity<MemberSearchResponse> addFollow(@AuthenticationPrincipal Member member,
-		@RequestParam String nickname) {
-		return ResponseEntity.ok(followService.createFollow(member, nickname));
+	@GetMapping("/{toId}/check")
+	public ResponseEntity<Boolean> getFollowCheck(
+		@AuthenticationPrincipal Member member,
+		@PathVariable Long toId) {
+		return ResponseEntity.ok(followService.readFollowCheck(member.getId(), toId));
+	}
+
+	@GetMapping("/count")
+	public ResponseEntity<FollowCountDto> getFollowCount(
+		@AuthenticationPrincipal Member member) {
+		return ResponseEntity.ok(followService.readFollowCount(member.getId()));
 	}
 
 	@PostMapping("/{toId}")
-	public ResponseEntity<Void> addFollow(@AuthenticationPrincipal Member member,
+	public ResponseEntity<Void> addFollow(
+		@AuthenticationPrincipal Member member,
 		@PathVariable Long toId) {
-		followService.createFollow(member, toId);
+		followService.updateFollow(member.getId(), toId, true);
 		return ResponseEntity.ok().build();
 	}
 
 	@DeleteMapping("/{toId}")
-	public ResponseEntity<Void> removeFollow(@AuthenticationPrincipal Member member,
+	public ResponseEntity<Void> removeFollow(
+		@AuthenticationPrincipal Member member,
 		@PathVariable Long toId) {
-		followService.deleteFollow(member, toId);
+		followService.updateFollow(member.getId(), toId, false);
 		return ResponseEntity.ok().build();
 	}
 }
