bb107dc39093958f95e05d72f1e022a716ef5cab
takealook97 <takealook97@naver.com>
2024-01-29 16:22:42 +0900

    refactor: service에서 jpa transaction 사용하도록 수정

--- backend/src/main/java/com/ssafy/jansorry/favorite/service/FavoriteService.java
+++ backend/src/main/java/com/ssafy/jansorry/favorite/service/FavoriteService.java
@@ -1,16 +1,12 @@
 package com.ssafy.jansorry.favorite.service;
 
-import static com.ssafy.jansorry.exception.ErrorCode.*;
-
 import java.time.LocalDateTime;
 import java.util.HashSet;
-import java.util.List;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ssafy.jansorry.exception.BaseException;
 import com.ssafy.jansorry.favorite.dto.FavoriteDto;
 import com.ssafy.jansorry.favorite.dto.FavoriteInfoDto;
 import com.ssafy.jansorry.favorite.repository.FavoriteRepository;
@@ -27,9 +23,7 @@ public class FavoriteService {
 	// 해당 대응의 좋아요 개수를 반환하는 메서드
 	public FavoriteInfoDto readFavoriteInfo(Long actionId, Long memberId) {
 		String key = actionId.toString();
-		favoriteRedisTemplate.watch(key);// redis에서 데이터를 읽기 전에 WATCH 설정
 		FavoriteDto favoriteDto = getFavoriteDto(key);
-		favoriteRedisTemplate.multi();// WATCH 이후에 데이터가 수정되면 WATCH가 실패하므로 다시 시도
 
 		// redis에 존재하지 않는 데이터라면 개수는 0개
 		Long favoriteCount = favoriteDto == null ? 0L : (long)favoriteDto.getMemberIdSet().size();
@@ -50,60 +44,27 @@ public class FavoriteService {
 
 	// redis 좋아요 테이블을 업데이트하는 메서드
 	public void updateFavorite(Long actionId, Long memberId, boolean isCreate) {
-		final int MAX_RETRIES = 3; // 최대 재시도 횟수 설정
-		int attempts = 0;          // 현재 시도 횟수
-		boolean success = false;   // 성공 여부
-
 		String key = actionId.toString();
+		FavoriteDto favoriteDto = getFavoriteDto(key);
 
-		while (!success && attempts < MAX_RETRIES) {
-			try {
-				favoriteRedisTemplate.watch(key); // Redis에서 데이터를 읽기 전에 WATCH 설정
-
-				FavoriteDto favoriteDto = getFavoriteDto(key);
-
-				favoriteRedisTemplate.multi(); // WATCH 이후에 데이터가 수정되면 WATCH가 실패하므로 다시 시도
-
-				if (isCreate) {
-					// 좋아요 추가
-					if (favoriteDto == null) {
-						favoriteDto = FavoriteDto.builder()
-							.memberIdSet(new HashSet<>())
-							.updatedAt(LocalDateTime.now())
-							.build();
-					}
-					favoriteDto.addFavorite(memberId);
-				} else {
-					// 좋아요 취소
-					if (favoriteDto == null) {
-						return; // Redis에 키가 없으면 (즉, favoriteDto가 null이면) 바로 리턴
-					}
-					favoriteDto.removeFavorite(memberId);
-				}
-
-				updateFavoriteDto(key, favoriteDto); // 좋아요 업데이트
-
-				List<Object> results = favoriteRedisTemplate.exec();
-				success = results != null; // 트랜잭션이 성공적으로 커밋되었는지 확인
-
-				if (success) {
-					return;
-				}
-				attempts++;
-				Thread.sleep(100); // 재시도 전에 짧은 지연
-			} catch (Exception e) {
-				attempts++;
-				if (attempts >= MAX_RETRIES) {
-					throw new BaseException(ATTEMPTS_OVERFLOW);// 재시도 횟수 초과
-				}
-			} finally {
-				favoriteRedisTemplate.unwatch(); // 트랜잭션이 성공하거나 모든 재시도가 실패한 경우 watch 해제
+		if (isCreate) {
+			// 좋아요 추가
+			if (favoriteDto == null) {
+				favoriteDto = FavoriteDto.builder()
+					.memberIdSet(new HashSet<>())
+					.updatedAt(LocalDateTime.now())
+					.build();
 			}
+			favoriteDto.addFavorite(memberId);
+			updateFavoriteDto(key, favoriteDto); // 좋아요 업데이트
+			return;
 		}
-
-		if (!success) {
-			throw new BaseException(ATTEMPTS_OVERFLOW);// 재시도 횟수 초과
+		// 좋아요 취소
+		if (favoriteDto == null) {
+			return; // Redis에 키가 없으면 (즉, favoriteDto가 null이면) 바로 리턴
 		}
+		favoriteDto.removeFavorite(memberId);
+		updateFavoriteDto(key, favoriteDto); // 좋아요 업데이트
 	}
 
 	// redis 로부터 해당 FavoriteDto 를 반환하는 메서드
--- backend/src/main/java/com/ssafy/jansorry/follow/service/FollowService.java
+++ backend/src/main/java/com/ssafy/jansorry/follow/service/FollowService.java
@@ -1,15 +1,11 @@
 package com.ssafy.jansorry.follow.service;
 
-import static com.ssafy.jansorry.exception.ErrorCode.*;
-
 import java.time.LocalDateTime;
 import java.util.HashSet;
-import java.util.List;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import com.ssafy.jansorry.exception.BaseException;
 import com.ssafy.jansorry.follow.dto.FollowCountDto;
 import com.ssafy.jansorry.follow.dto.FollowDto;
 import com.ssafy.jansorry.member.repository.MemberRepository;
@@ -67,70 +63,41 @@ public class FollowService {
 	 * follower:toId : Set<memberId>, updatedAt
 	 */
 	public void updateFollow(Long fromId, Long toId, boolean isCreate) {
-		final int MAX_RETRIES = 3; // 최대 재시도 횟수 설정
-		int attempts = 0;          // 현재 시도 횟수
-		boolean success = false;   // 성공 여부
-
 		String followingKey = FOLLOWING + fromId.toString();
 		String followerKey = FOLLOWER + toId.toString();
 
-		while (!success && attempts < MAX_RETRIES) {
-			try {
-				followRedisTemplate.watch(followingKey);
-				followRedisTemplate.watch(followerKey); // Redis에서 데이터를 읽기 전에 WATCH 설정
+		FollowDto followingDto = getFollowDto(followingKey);
+		FollowDto followerDto = getFollowDto(followerKey);
 
-				FollowDto followingDto = getFollowDto(followingKey);
-				FollowDto followerDto = getFollowDto(followerKey);
-
-				followRedisTemplate.multi(); // WATCH 이후에 데이터가 수정되면 WATCH가 실패하므로 다시 시도
-
-				if (isCreate) {// 팔로우 추가
-					if (followingDto == null) {
-						followingDto = FollowDto.builder()
-							.memberIdSet(new HashSet<>())
-							.updatedAt(LocalDateTime.now())
-							.build();
-					}
-					if (followerDto == null) {
-						followerDto = FollowDto.builder()
-							.memberIdSet(new HashSet<>())
-							.updatedAt(LocalDateTime.now())
-							.build();
-					}
-					followingDto.addFollow(toId); // fromId의 팔로잉 목록에 toId 추가
-					followerDto.addFollow(fromId); // toId의 팔로워 목록에 fromId 추가 (양방향 저장)
-				} else {// 팔로우 취소
-					if (followingDto == null || followerDto == null) {
-						return; // Redis에 키가 없으면 (즉, DTO가 null이면) 바로 리턴
-					}
-					followingDto.removeFollow(toId); // 해당 대상을 삭제
-					followerDto.removeFollow(fromId); // 해당 주체를 삭제
-				}
-
-				updateFollowDto(followingKey, followingDto); // 팔로잉 업데이트
-				updateFollowDto(followerKey, followerDto); // 팔로워 업데이트 (양방향 업데이트)
-
-				List<Object> results = followRedisTemplate.exec();
-				success = results != null; // 트랜잭션이 성공적으로 커밋되었는지 확인
-
-				if (success) {
-					return;
-				}
-				attempts++;
-				Thread.sleep(100); // 재시도 전에 짧은 지연
-			} catch (Exception e) {
-				attempts++;
-				if (attempts >= MAX_RETRIES) {
-					throw new BaseException(ATTEMPTS_OVERFLOW);// 재시도 횟수 초과
-				}
-			} finally {
-				followRedisTemplate.unwatch(); // 트랜잭션이 성공하거나 모든 재시도가 실패한 경우 watch 해제
+		if (isCreate) {// 팔로우 추가
+			if (followingDto == null) {
+				followingDto = FollowDto.builder()
+					.memberIdSet(new HashSet<>())
+					.updatedAt(LocalDateTime.now())
+					.build();
 			}
-		}
+			if (followerDto == null) {
+				followerDto = FollowDto.builder()
+					.memberIdSet(new HashSet<>())
+					.updatedAt(LocalDateTime.now())
+					.build();
+			}
+			followingDto.addFollow(toId); // fromId의 팔로잉 목록에 toId 추가
+			followerDto.addFollow(fromId); // toId의 팔로워 목록에 fromId 추가 (양방향 저장)
 
-		if (!success) {
-			throw new BaseException(ATTEMPTS_OVERFLOW);// 재시도 횟수 초과
+			updateFollowDto(followingKey, followingDto); // 팔로잉 업데이트
+			updateFollowDto(followerKey, followerDto); // 팔로워 업데이트 (양방향 업데이트)
+			return;
 		}
+		// 팔로우 취소
+		if (followingDto == null || followerDto == null) {
+			return; // Redis에 키가 없으면 (즉, DTO가 null이면) 바로 리턴
+		}
+		followingDto.removeFollow(toId); // 해당 대상을 삭제
+		followerDto.removeFollow(fromId); // 해당 주체를 삭제
+
+		updateFollowDto(followingKey, followingDto); // 팔로잉 업데이트
+		updateFollowDto(followerKey, followerDto); // 팔로워 업데이트 (양방향 업데이트)
 	}
 
 	// redis 로부터 해당 FollowDto 를 반환하는 메서드
