917a2db869c7f794ed1e5b00c2ed028895bf3c6d
sunju5402 <ssonju5402@gmail.com>
2024-01-29 17:48:38 +0900

    fix: 카테고리 response body 변경

--- backend/src/main/java/com/ssafy/jansorry/nag/controller/NagController.java
+++ backend/src/main/java/com/ssafy/jansorry/nag/controller/NagController.java
@@ -3,13 +3,12 @@ package com.ssafy.jansorry.nag.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.jansorry.member.domain.Member;
+import com.ssafy.jansorry.nag.dto.CategoryDto;
 import com.ssafy.jansorry.nag.dto.NagDto;
 import com.ssafy.jansorry.nag.service.NagService;
 
@@ -27,7 +26,7 @@ public class NagController {
 	}
 
 	@GetMapping
-	private ResponseEntity<List<NagDto>> getAllNags() {
+	private ResponseEntity<List<CategoryDto>> getAllNags() {
 		return ResponseEntity.ok(nagService.readAllNags());
 	}
 }
