614cedb98f5b5a9c43693bf21cb3302b4d08a888
sunju5402 <ssonju5402@gmail.com>
2024-01-29 12:39:54 +0900

    feat: 10/20/30대별 피드 조회 api 생성
    Related to: #S10P12A107-156

--- backend/src/main/java/com/ssafy/jansorry/feed/controller/FeedController.java
+++ backend/src/main/java/com/ssafy/jansorry/feed/controller/FeedController.java
@@ -2,7 +2,6 @@ package com.ssafy.jansorry.feed.controller;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +24,18 @@ public class FeedController {
 	@GetMapping("/live")
 	public ResponseEntity<Slice<FeedInfoResponse>> getLiveFeeds(
 		@RequestParam(required = false) Long lastActionId,
-		@PageableDefault(size = 3) Pageable pageable
+		Pageable pageable
 	) {
 		return ResponseEntity.ok(feedService.readLiveFeeds(lastActionId, pageable));
+	}
+
+	// 10대, 20대, 30대 피드 조회
+	@GetMapping("/generation")
+	public ResponseEntity<Slice<FeedInfoResponse>> getTeenageFeeds(
+		@RequestParam(required = false) Long lastActionId,
+		@RequestParam int age,
+		Pageable pageable
+	) {
+		return ResponseEntity.ok(feedService.readGenerationFeeds(lastActionId, age, pageable));
 	}
 }
