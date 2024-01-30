6201348f153c4d7aa116c53c583ffce5a037abde
takealook97 <takealook97@naver.com>
2024-01-30 15:16:12 +0900

    feat: favorite custom repository 생성 및 구현
    query dsl을 활용하여 redis 에서 mysql로 데이터를 반영하는 메서드 구현

---
+++ backend/src/main/java/com/ssafy/jansorry/favorite/repository/FavoriteRepositoryImpl.java
@@ -0,0 +1,70 @@
+package com.ssafy.jansorry.favorite.repository;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.jansorry.action.domain.Action;
+import com.ssafy.jansorry.action.repository.ActionRepository;
+import com.ssafy.jansorry.favorite.domain.Favorite;
+import com.ssafy.jansorry.favorite.domain.QFavorite;
+import com.ssafy.jansorry.member.repository.MemberRepository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class FavoriteRepositoryImpl implements FavoriteCustomRepository {
+	private final JPAQueryFactory queryFactory;
+	private final EntityManager entityManager;
+	private final MemberRepository memberRepository;
+	private final ActionRepository actionRepository;
+
+	@Override
+	public void updateFavoritesByActionId(Long actionId, Set<Long> updatedMemberIds) {
+		QFavorite favorite = QFavorite.favorite;
+
+		// 1. MySQL에서 Redis에 없는 데이터 삭제
+		queryFactory.delete(favorite)
+			.where(favorite.action.id.eq(actionId)
+				.and(favorite.member.id.notIn(updatedMemberIds)))
+			.execute();
+
+		// 2. Redis에서 MySQL에 없는 데이터 추가
+		// 이를 위해 MySQL에서 해당 actionId의 모든 memberId를 가져온다.
+		List<Long> existingMemberIds = queryFactory.select(favorite.member.id)
+			.from(favorite)
+			.where(favorite.action.id.eq(actionId))
+			.fetch();
+
+		// Redis에는 있지만 MySQL에 없는 memberId를 찾아 새 Favorite 인스턴스를 생성합니다.
+		Set<Long> memberIdsToAdd = updatedMemberIds.stream()
+			.filter(id -> !existingMemberIds.contains(id))
+			.collect(Collectors.toSet());
+
+		// Action 엔티티 조회
+		Action action = actionRepository.findById(actionId).orElse(null);
+		if (action == null) {// 해당 대응을 찾을 수 없는 경우 리턴
+			return;
+		}
+
+		// 새로운 Favorite 인스턴스 생성
+		List<Favorite> favoritesToAdd = updatedMemberIds.stream()
+			.filter(id -> !existingMemberIds.contains(id))
+			.map(memberId -> memberRepository.findById(memberId).orElse(null))
+			.filter(Objects::nonNull)
+			.map(member -> Favorite.builder()
+				.member(member)
+				.action(action)
+				.build())
+			.toList();
+
+		// 새로운 Favorite 인스턴스 저장
+		favoritesToAdd.forEach(entityManager::persist);// 배치 삽입 최적화
+	}
+}
