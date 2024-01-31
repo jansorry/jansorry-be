282bcffa8dafaf30ca38c5465cc37a292fae0506
takealook97 <takealook97@naver.com>
2024-01-31 10:44:21 +0900

    refactor: follow custom repository 파라미터 명 수정

--- backend/src/main/java/com/ssafy/jansorry/follow/repository/FollowCustomRepository.java
+++ backend/src/main/java/com/ssafy/jansorry/follow/repository/FollowCustomRepository.java
@@ -3,5 +3,5 @@ package com.ssafy.jansorry.follow.repository;
 import java.util.Set;
 
 public interface FollowCustomRepository {
-	void updateFollowsByFromId(Long memberId, Set<Long> updatedMemberIds);
+	void updateFollowsByFromId(Long fromId, Set<Long> updatedMemberIds);
 }
