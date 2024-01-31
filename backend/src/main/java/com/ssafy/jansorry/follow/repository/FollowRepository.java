bdd1f2527038457a9e29869b5c0da5fceed7df32
takealook97 <takealook97@naver.com>
2024-01-31 10:44:00 +0900

    remove: follow repository 미사용 메서드 삭제

--- backend/src/main/java/com/ssafy/jansorry/follow/repository/FollowRepository.java
+++ backend/src/main/java/com/ssafy/jansorry/follow/repository/FollowRepository.java
@@ -1,16 +1,12 @@
 package com.ssafy.jansorry.follow.repository;
 
-import java.util.List;
-
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.jansorry.follow.domain.Follow;
-import com.ssafy.jansorry.member.domain.Member;
 
 @Repository
+@Primary
 public interface FollowRepository extends JpaRepository<Follow, Long>, FollowCustomRepository {
-	Long countByToId(Long toId);
-
-	List<Follow> findAllByMember(Member member);
 }
