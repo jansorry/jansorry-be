0748c43f708f6c2e847a72de11c7424d6592b519
takealook97 <takealook97@naver.com>
2024-02-05 17:27:37 +0900

    refactor: gpt 를 통해 title 생성 시 잔소리를 파라미터로 받도록 수정

--- backend/src/main/java/com/ssafy/jansorry/receipt/util/MessageGenerator.java
+++ backend/src/main/java/com/ssafy/jansorry/receipt/util/MessageGenerator.java
@@ -6,7 +6,8 @@ public class MessageGenerator {
 	static final String DESCRIPTION = "님이 청구하신 잔소리 영수증을 확인해보세요.";
 	static final String URL_MESSAGE = "https://jansorry.com";
 
-	public static OpenGraphMessage generateMessage(String name) {
+	public static OpenGraphMessage generateMessage(String name, String nagContent) {
+		// todo: nagContent(잔소리)로 title을 생성한다.
 		String title = "";
 
 		return OpenGraphMessage.builder()
