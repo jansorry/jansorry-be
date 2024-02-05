d2162d4d7c917686dd4026eaca24097fee809e18
sunju5402 <ssonju5402@gmail.com>
2024-02-05 13:56:17 +0900

    feat: 카카오 닉네임 추가
    Related to: #S10P12A107-330

--- backend/src/main/java/com/ssafy/jansorry/member/util/MemberMapper.java
+++ backend/src/main/java/com/ssafy/jansorry/member/util/MemberMapper.java
@@ -3,11 +3,13 @@ package com.ssafy.jansorry.member.util;
 import com.ssafy.jansorry.member.domain.Member;
 import com.ssafy.jansorry.member.domain.OauthId;
 import com.ssafy.jansorry.member.domain.type.OauthServerType;
+import com.ssafy.jansorry.member.dto.KakaoMemberResponse;
 
 public class MemberMapper {
-	public static Member toMember(Long id) {
+	public static Member toMember(KakaoMemberResponse kakaoMemberResponse) {
 		return Member.builder()
-			.oauthId(new OauthId(String.valueOf(id), OauthServerType.KAKAO))
+			.oauthId(new OauthId(String.valueOf(kakaoMemberResponse.id()), OauthServerType.KAKAO))
+			.name(kakaoMemberResponse.kakaoAccount().profile().nickname())
 			.build();
 	}
 
