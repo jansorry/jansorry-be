85701cc7f45699656aa3c4d043c68f5a76de7d99
takealook97 <takealook97@naver.com>
2024-02-05 10:11:10 +0900

    refactor: message generator 를 활용하여 entity를 구성하도록 수정

--- backend/src/main/java/com/ssafy/jansorry/receipt/util/ReceiptMapper.java
+++ backend/src/main/java/com/ssafy/jansorry/receipt/util/ReceiptMapper.java
@@ -2,29 +2,32 @@ package com.ssafy.jansorry.receipt.util;
 
 import com.ssafy.jansorry.member.domain.Member;
 import com.ssafy.jansorry.receipt.domain.Receipt;
-import com.ssafy.jansorry.receipt.dto.ReceiptDto;
+import com.ssafy.jansorry.receipt.dto.OpenGraphMessage;
+import com.ssafy.jansorry.receipt.dto.ReceiptResponse;
+import com.ssafy.jansorry.receipt.dto.ReceiptSaveDto;
 
 public class ReceiptMapper {
-	public static ReceiptDto toDto(Receipt receipt){
-		return ReceiptDto.builder()
+	public static ReceiptResponse toDto(Receipt receipt){
+		return ReceiptResponse.builder()
 			.title(receipt.getTitle())
 			.description(receipt.getDescription())
 			.message(receipt.getMessage())
 			.familyUrl(receipt.getFamilyUrl())
 			.friendUrl(receipt.getFriendUrl())
 			.totalPrice(receipt.getTotalPrice())
+			.createdAt(receipt.getCreatedAt())
 			.build();
 	}
 
-	public static Receipt toEntity(ReceiptDto receiptDto, Member member){
+	public static Receipt toEntity(ReceiptSaveDto receiptSaveDto, Member member, OpenGraphMessage openGraphMessage){
 		return Receipt.builder()
 			.member(member)
-			.title(receiptDto.title())
-			.description(receiptDto.description())
-			.message(receiptDto.message())
-			.familyUrl(receiptDto.familyUrl())
-			.friendUrl(receiptDto.friendUrl())
-			.totalPrice(receiptDto.totalPrice())
+			.title(openGraphMessage.title())// 생성
+			.description(openGraphMessage.description())// 생성
+			.message(openGraphMessage.message())// 생성
+			.familyUrl(receiptSaveDto.familyUrl())
+			.friendUrl(receiptSaveDto.friendUrl())
+			.totalPrice(receiptSaveDto.totalPrice())
 			.deleted(false)
 			.build();
 	}
