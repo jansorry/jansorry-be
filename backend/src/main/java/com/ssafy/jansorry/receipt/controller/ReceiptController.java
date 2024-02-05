0cda55abf28e5c8350d4be53c4732b229fb59301
takealook97 <takealook97@naver.com>
2024-02-05 10:10:49 +0900

    feat: 영수증 저장 및 조회 시 dto 분리

--- backend/src/main/java/com/ssafy/jansorry/receipt/controller/ReceiptController.java
+++ backend/src/main/java/com/ssafy/jansorry/receipt/controller/ReceiptController.java
@@ -1,7 +1,5 @@
 package com.ssafy.jansorry.receipt.controller;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.jansorry.member.domain.Member;
-import com.ssafy.jansorry.receipt.dto.ReceiptDto;
-import com.ssafy.jansorry.receipt.dto.ReceiptRankDto;
+import com.ssafy.jansorry.receipt.dto.ReceiptResponse;
+import com.ssafy.jansorry.receipt.dto.ReceiptSaveDto;
 import com.ssafy.jansorry.receipt.service.ReceiptService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -36,10 +34,10 @@ public class ReceiptController {
 	@PostMapping
 	public ResponseEntity<Long> addReceipt(    //ResponseEntity: 결과 데이터와 HTTP 상태 코드를 직접 제어할 수 있는 클래스
 		@AuthenticationPrincipal Member member,
-		@RequestBody ReceiptDto receiptDto    //@RequestBody: HTTP 요청 바디 값을 컨트롤러 메서드의 매개 변수로 받을 수 있음
+		@RequestBody ReceiptSaveDto receiptSaveDto    //@RequestBody: HTTP 요청 바디 값을 컨트롤러 메서드의 매개 변수로 받을 수 있음
 	) {
 
-		return ResponseEntity.ok(receiptService.createReceipt(receiptDto, member));
+		return ResponseEntity.ok(receiptService.createReceipt(receiptSaveDto, member));
 	}
 
 	// 영수증 개수 확인 API - 영수증 개수를 0~3까지 반환
@@ -58,12 +56,12 @@ public class ReceiptController {
 		summary = "영수증 확인",
 		description = "해당 번호의 영수증을 상세 조회한다.")
 	@GetMapping("/{seq}")
-	public ResponseEntity<ReceiptDto> getReceipt(
+	public ResponseEntity<ReceiptResponse> getReceipt(
 		@AuthenticationPrincipal Member member,    //로그인 세션 정보
 		@PathVariable("seq") Long seq
 	) {
-		ReceiptDto receiptDto = receiptService.readReceipt(member, seq - 1);
-		return ResponseEntity.ok(receiptDto);
+		ReceiptResponse receiptResponse = receiptService.readReceipt(member, seq - 1);
+		return ResponseEntity.ok(receiptResponse);
 	}
 
 	// 영수증 삭제하는 API - 3개 중 {seq}번째의 영수증을 반환
@@ -77,10 +75,5 @@ public class ReceiptController {
 	) {
 		receiptService.deleteReceipt(member, seq - 1);
 		return ResponseEntity.ok().build();
-	}
-
-	@GetMapping("/rank")
-	public ResponseEntity<List<ReceiptRankDto>> getRanks() {
-		return ResponseEntity.ok(receiptService.getTopReceiptsList());
 	}
 }
